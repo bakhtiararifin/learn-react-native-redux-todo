6f1beee596aa6b984b818e212e9b91a16e044c81
Bakhtiar Arifin <bakhtiararifin.88@gmail.com>
2017-09-20 09:32:52 +0700

    todoService

--- components/TodoForm.js
+++ components/TodoForm.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, Text } from 'react-native';
 import { Button } from 'react-native-elements';
 import { Actions } from 'react-native-router-flux';
 
+import { addTodo } from '../services/todoService';
+
 class TodoForm extends Component {
-  state = { todo: '' };
+  state = {
+    todo: '',
+    error: null,
+    loading: false
+  };
 
   onPressAdd = () => {
-    fetch('https://lumen-todo-api.herokuapp.com/todos/add', {
-      method: 'post',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({todo: this.state.todo})
-    })
-    .then(response => response.json())
-    .then(response => {
-      Actions.todoList();
-    })
+    this.setState({ ...this.state, loading: true });
+
+    addTodo(this.state.todo)
+      .then(response => {
+        if (response.ok) {
+          Actions.todoList();
+        } else {
+          response.json().then(json => {
+            this.setState({ ...this.state, error: json.todo[0], loading: false })
+          })
+        }
+      })
   }
 
   render() {
@@ -28,13 +34,21 @@ class TodoForm extends Component {
       buttonStyle
     } = styles;
 
+    let text = this.state.loading ? 'Loading...' :
+                this.state.error ? this.state.error : null;
+
     return (
-      <View style={{ backgroundColor: '#ddd' }}>
+      <View style={{ flex: 1, backgroundColor: '#ddd' }}>
         <View style={containerStyle}>
           <TextInput
             onChangeText={todo => this.setState({ todo })}
             value={this.state.todo}
           />
+          {text ? (
+            <Text style={{ color: 'red', textAlign: 'center', marginBottom: 5 }}>
+              {text}
+            </Text>
+          ) : null}
           <Button
             buttonStyle={buttonStyle}
             title="Add"
--- components/TodoList.js
+++ components/TodoList.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { Button, List, ListItem } from 'react-native-elements';
 import { Actions } from 'react-native-router-flux';
 
+import { getTodos, deleteTodo } from '../services/todoService';
+
 class TodoList extends Component {
+  state = {
+    loading: false
+  }
+
   componentDidMount() {
-    this.loadTodos()
+    this.setState({ loading: true });
+    getTodos()
       .then(todos => {
         this.props.setTodos(todos)
+        this.setState({ loading: false });
       })
   }
 
@@ -15,29 +23,30 @@ class TodoList extends Component {
     return fetch('https://lumen-todo-api.herokuapp.com/todos', {
       method: 'post'
     })
-    .then(response => response.json())
-    .then(data => data.map(x => ({
-      id: x.id,
-      todo: x.todo
-    })))
+      .then(response => response.json())
+      .then(data => data.map(x => ({
+        id: x.id,
+        todo: x.todo
+      })))
   }
 
   onPressItem = id => {
-    fetch('https://lumen-todo-api.herokuapp.com/todos/delete', {
-      method: 'post',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ id })
-    })
-    .then(this.loadTodos)
-    .then(todos => {
-      this.props.setTodos(todos)
-    })
+    if (this.state.loading) {
+      return;
+    }
+
+    this.setState({ loading: true });
+
+    deleteTodo(id)
+      .then(getTodos)
+      .then(todos => {
+        this.props.setTodos(todos)
+        this.setState({ loading: false });
+      })
   }
 
   render() {
+    let loading = this.state.loading;
     return (
       <View style={{ flex: 1, backgroundColor: '#ddd' }}>
         <View style={{ marginTop: 10 }}>
@@ -46,18 +55,23 @@ class TodoList extends Component {
             onPress={() => Actions.todoForm()}
           />
         </View>
-        <List containerStyle={{marginTop: 10}}>
-            {
-              this.props.todos.map((todo) => (
-                <ListItem
-                  key={todo.id}
-                  title={todo.todo}
-                  onPress={() => this.onPressItem(todo.id)}
-                />
-              ))
-            }
-          </List>
-        </View>
+        {loading ? (
+          <Text style={{ color: 'red', textAlign: 'center' }}>
+            Loading...
+          </Text>
+        ) : null}
+        <List containerStyle={{ marginBottom: 10 }}>
+          {
+            this.props.todos.map((todo) => (
+              <ListItem
+                key={todo.id}
+                title={todo.todo}
+                onPress={() => this.onPressItem(todo.id)}
+              />
+            ))
+          }
+        </List>
+      </View>
     );
   }
 }
